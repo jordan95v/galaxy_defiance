09d96a57c13d3787fce819f97c764284843de8e3
jordan95v <jordan.dfrsne@gmail.com>
2024-03-18 14:30:55 +0100

    Make the background move faster as the game goes.

--- scenes/ui/space_background/space_background.gd
+++ scenes/ui/space_background/space_background.gd
@@ -4,10 +4,21 @@ extends ParallaxBackground
 @onready var ring_planet: ParallaxLayer = $RingPlanet
 @onready var far_planets: ParallaxLayer = $FarPlanets
 @onready var stars: ParallaxLayer = $Stars
+@onready var timer: Timer = $Timer
+var speed: int = 20
+@export var acceleration: int = 10
+
+
+func _ready() -> void:
+	timer.timeout.connect(on_timer_timeout)
+
 
 func _process(delta: float) -> void:
-	stars.motion_offset.y += 20 * delta
-	far_planets.motion_offset.y += 15 * delta
-	ring_planet.motion_offset.y += 6 * delta
-	big_planet.motion_offset.y += 4 * delta
+	stars.motion_offset.y += speed * 0.80 * delta
+	far_planets.motion_offset.y += speed * 0.60 * delta
+	ring_planet.motion_offset.y += speed * 0.30 * delta
+	big_planet.motion_offset.y += speed * 0.10 * delta	
 	
+	
+func on_timer_timeout() -> void:
+	speed = clamp(speed + acceleration, speed, speed * 20)
