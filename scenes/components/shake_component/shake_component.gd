00d2e28d69007d0db6fa6ffd733440e1ccab45d2
jordan95v <jordan.dfrsne@gmail.com>
2024-03-18 14:38:33 +0100

    Reworked assets folder.

--- scenes/components/shake_component/shake_component.gd
+++ scenes/components/shake_component/shake_component.gd
@@ -3,10 +3,12 @@ extends Node
 
 var shake_strength: float = 0
 @export var sprite: AnimatedSprite2D
+@export var min_shake_strength: int = 3
+@export var max_shake_strength: int = 6
 
 
 func apply_random_shake() -> void:
-	shake_strength = randf_range(3, 6)
+	shake_strength = randf_range(min_shake_strength, max_shake_strength)
 
 
 func _process(_delta: float) -> void:
@@ -16,4 +18,4 @@ func _process(_delta: float) -> void:
 		randf_range(-shake_strength, shake_strength),
 		randf_range(-shake_strength, shake_strength)		
 	)
-	shake_strength = move_toward(shake_strength, 0, 0.5)
+	shake_strength = move_toward(shake_strength, 0, 1)
